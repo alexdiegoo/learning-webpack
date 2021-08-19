63a39ad5932960832ea6b4ddb8c7ee084f44c8ce
Alex Diego <alex.diegosn10@gmail.com>
2021-08-18 22:21:18 -0300

    Configurando css loader

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
+import '../css/styles.scss';
 const textComponent = () => {
   const elH1 = document.createElement('h1');
   elH1.innerHTML = "Olá mundo!";
+  elH1.classList.add('title');
   return elH1
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
@@ -7,10 +8,25 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss/,
+        use: [
+          miniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
   plugins: [
     new htmlWebpackPlugin({
       filename: 'index.html',
       template: './src/index.html'
+    }),
+    new miniCssExtractPlugin({
+      filename: 'styles.css'
     })
   ]
 }
